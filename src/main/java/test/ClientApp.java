e712ad46edd22de093f2323bd897ff02291f14da
MAHESH <maheshshinde9100@gmail.com>
2025-03-13 20:41:49 +0530

    Successfully performed insertion operation in mysql db table -- from clientApp.java (user input get stored in 'sp_student' table)

--- src/main/java/test/ClientApp.java
+++ src/main/java/test/ClientApp.java
@@ -6,6 +6,7 @@ import com.di.controller.StudentController;
 
 public class ClientApp {
     public static void main(String[] args) {
+        //Container obj creation
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         StudentController controller = context.getBean("stController", StudentController.class);
         System.out.println(controller.process("101", "mahesh", "67", "10000", "1000"));
