fbbb1ccb21fac0f3551c50afe24d7ffe3c4739ae
MAHESH <maheshshinde9100@gmail.com>
2025-03-13 21:10:10 +0530

    Main  file changes -- data types of values now matching with db attributes successfully

--- src/main/java/test/ClientApp.java
+++ src/main/java/test/ClientApp.java
@@ -9,6 +9,9 @@ public class ClientApp {
         //Container obj creation
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         StudentController controller = context.getBean("stController", StudentController.class);
-        System.out.println(controller.process("101", "mahesh", "67", "10000", "1000"));
+        // System.out.println(controller.process("101", "mahesh", "67", "10000", "1000"));
+        // System.out.println(controller.process("102", "jon", "35", "3000", "100"));
+        System.out.println(controller.process("103", "robb", "50", "4000", "10"));
+
     }
 }
