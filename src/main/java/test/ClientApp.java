5bfac08bda1bce073112062dd585e87853c2bdd1
MAHESH <maheshshinde9100@gmail.com>
2025-03-13 20:29:07 +0530

    db connectivity

--- src/main/java/test/ClientApp.java
+++ src/main/java/test/ClientApp.java
@@ -1,11 +1,13 @@
 package test;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import com.di.controller.*;
+import com.di.controller.StudentController;
+
 public class ClientApp {
-    public static void main(String[] args){
-        ApplicationContext  context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        StudentController controller = context.getBean("stController",StudentController.class);
-        System.out.println(controller.process("101","mahesh","67","10000","1000"));
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        StudentController controller = context.getBean("stController", StudentController.class);
+        System.out.println(controller.process("101", "mahesh", "67", "10000", "1000"));
     }
 }
